49348501322ed4ca696c7fa6680194c57c640da5
Zijing-Y <uceszy2@ucl.ac.uk>
2021-12-27 00:47:35 +0000

    task3.py: read itn.json and convert to direction

--- task3.py
+++ task3.py
@@ -1,4 +1,9 @@
 
 # task3 nearest integrated transport network
+import json
 from rtree import index
-
+idx = index.Index()
+# read the ITN file
+with open('Material/Material/itn/solent_itn.json', 'r') as f:
+    itn = json.load(f)
+# print(itn)
