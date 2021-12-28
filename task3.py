201e1dbfd9e13739c8d88331c00a55900186b920
Zijing-Y <uceszy2@ucl.ac.uk>
2021-12-28 14:42:29 +0000

    task3.py: add index.nearest

--- task3.py
+++ task3.py
@@ -7,4 +7,22 @@ idx = index.Index()
 with open('Material/Material/itn/solent_itn.json', 'r') as f:
     itn = json.load(f)
 # print(itn)
+# print(itn.keys())
+road_node = itn['roadnodes']
+# print(road_node)
+# print(road_node.values())
+for i in road_node.values():
+    roadnodes = i['coords']
 
+for n, point in enumerate(roadnodes):
+    idx.insert(n, point, str(n))
+
+# define a query for user:
+q_user = (qu_x, qu_y)
+# identify the nearest itn node to the user
+index.nearest(q_user, num_results=1, objects=True)
+
+# define a query for highest point
+q_highest = (qh_x, qh_y)
+# identify the nearest itn node to the highest
+index.nearest(q_highest, num_results=1, objects=True)
