3eb39e5b4103d9e798404e1402881c2dc93e8535
Zijing-Y <uceszy2@ucl.ac.uk>
2021-12-28 23:42:14 +0000

    task3.py: identify the nearest itn node to user and highest point

--- task3.py
+++ task3.py
@@ -1,6 +1,9 @@
 
 # task3 nearest integrated transport network
-import json
+# reference:
+# https://rtree.readthedocs.io/en/latest/class.html
+# http://www.manongjc.com/detail/7-bmdxrklxbxfzgon.html
+
 from rtree import index
 idx = index.Index()
 
@@ -10,22 +13,28 @@ with open('Material/Material/itn/solent_itn.json', 'r') as f:
 road_node = itn['roadnodes']
 
 # create roadnodes sequence:
-roadnodes = []
+road_nodes = []
 for i in road_node.values():
-    roadnodes.append(i['coords'])
-# print(roadnodes)
+    road_nodes.append(tuple(i['coords']))
 
 # add roadnodes and rectangles to the spatial index:
-for n, point in enumerate(roadnodes):
+for n, point in enumerate(road_nodes):
     idx.insert(n, point, str(n))
-# print(idx)
 
 # define a query for user:
-q_user = (qu_x, qu_y)
-# identify the nearest itn node to the user
-idx.nearest(q_user, num_results=1, objects=True)
+q_user = (point_user.x, point_user.y)
 
-# define a query for highest point
-q_highest = (qh_x, qh_y)
-# identify the nearest itn node to the highest
-idx.nearest(q_highest, num_results=1, objects=True)
+# identify the nearest itn node to the user
+# objects=False turn to id
+# objects=True turn to item
+hits_1 = list(idx.nearest(q_user, num_results=1, objects=False))
+for i in hits_1:
+    print('the nearest ITN node to the user', point_user, 'is:', road_nodes[i])
+
+# define a query for highest point:
+q_highest = (a[0], a[1])
+
+# identify the nearest itn node to the highest:
+hits_2 = list(idx.nearest(q_highest, num_results=1, objects=False))
+for i in hits_2:
+    print('the nearest ITN node to the highest point', a, 'is:', road_nodes[i])
