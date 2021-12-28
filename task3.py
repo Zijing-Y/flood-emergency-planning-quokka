d1251976de2dc2c264c0923a812d046b974880ac
Zijing-Y <uceszy2@ucl.ac.uk>
2021-12-28 17:23:47 +0000

    task3.py: add spatial index, define user and highest query

--- task3.py
+++ task3.py
@@ -3,27 +3,29 @@
 import json
 from rtree import index
 idx = index.Index()
+
 # read the ITN file
 with open('Material/Material/itn/solent_itn.json', 'r') as f:
     itn = json.load(f)
-# print(itn)
-# print(itn.keys())
 road_node = itn['roadnodes']
-# print(road_node)
-# print(road_node.values())
-for i in road_node.values():
-    roadnodes = i['coords']
 
+# create roadnodes sequence:
+roadnodes = []
+for i in road_node.values():
+    roadnodes.append(i['coords'])
+# print(roadnodes)
+
+# add roadnodes and rectangles to the spatial index:
 for n, point in enumerate(roadnodes):
     idx.insert(n, point, str(n))
+# print(idx)
 
 # define a query for user:
 q_user = (qu_x, qu_y)
 # identify the nearest itn node to the user
-index.nearest(q_user, num_results=1, objects=True)
+idx.nearest(q_user, num_results=1, objects=True)
 
 # define a query for highest point
 q_highest = (qh_x, qh_y)
 # identify the nearest itn node to the highest
-index.nearest(q_highest, num_results=1, objects=True)
-
+idx.nearest(q_highest, num_results=1, objects=True)
